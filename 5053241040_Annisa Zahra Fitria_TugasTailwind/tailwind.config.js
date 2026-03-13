1d05a9124da616a521bd50d0a0249a98a29b72ca
xoaaa <yaraxoaraal@gmail.com>
2026-03-13 19:32:06 +0700

    done

---
+++ 5053241040_Annisa Zahra Fitria_TugasTailwind/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./*.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2563EB',
+        secondary: '#16A34A',
+        background: '#F8FAFC',
+      },
+    },
+  },
+};
